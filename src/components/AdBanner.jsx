e03be7d2b2666c226fe15f76f9cd4d004db4ea17
kousenyou <forSchooldaze@outlook.jp>
2026-08-06 18:20:46 +0900

    Replace Ninja AdMax with A8.net ad

--- src/components/AdBanner.jsx
+++ src/components/AdBanner.jsx
@@ -1,3 +1,18 @@
 export default function AdBanner() {
-  return null;
+  return (
+    <div className="ad-container no-print" style={{
+      width: '100%',
+      minHeight: '60px',
+      margin: '1rem 0',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column'
+    }}>
+      <a href="https://px.a8.net/svt/ejp?a8mat=4B9ZDE+4QVG62+4NPS+609HT" rel="nofollow">
+        <img border="0" width="468" height="60" alt="" src="https://www24.a8.net/svt/bgt?aid=260806226287&wid=002&eno=01&mid=s00000021736001009000&mc=1" />
+      </a>
+      <img border="0" width="1" height="1" src="https://www17.a8.net/0.gif?a8mat=4B9ZDE+4QVG62+4NPS+609HT" alt="" />
+    </div>
+  );
 }
